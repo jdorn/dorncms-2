36ea2b3c8e664f0975e21e4f928bc06fc26499b1
Jeremy <jeremy@jeremydorn.com>
2014-03-29 22:10:41 -0700

    Moved some directories around.
    Now all content pages are completely separate from admin and framework code.
    This makes it easy to update the framework without touching your site code.
    
    Also added {sitedir}/routes.php file where you can define custom routes for you site.

--- index.php
+++ index.php
@@ -3,13 +3,63 @@ session_cache_limiter(false);
 session_start();
 require_once 'vendor/autoload.php'; 
 
-$config = json_decode(file_get_contents('config/config.json'));
+class DornCMSApp extends \Slim\Slim {
+  protected $string_view;
+  protected $site_view;
+  public $config;
+  public $meta_schema;
+  
+  public function __construct($args) {
+    parent::__construct($args);
+    
+    $this->config = json_decode(file_get_contents('config/config.json'));
+    
+    $site_loader = new \Twig_Loader_Filesystem($this->config->sitedir.'templates');
+    $this->site_view = new \Twig_Environment($site_loader);
+    $this->site_view->addExtension(new \Slim\Views\TwigExtension());
+    
+    $this->meta_schema = json_decode(file_get_contents('config/meta.schema.json'));
+  }
+  public function renderSitePage($page) {
+    // Get template data
+    $data = array(
+      'meta'=>json_decode(file_get_contents($this->config->sitedir.'pages/'.$page.'/meta.json')),
+      'content'=>json_decode(file_get_contents($this->config->sitedir.'pages/'.$page.'/content.json')),
+      'parameters'=>json_decode(file_get_contents($this->config->sitedir.'parameters.json'))
+    );
+    
+    $this->response->setBody($this->site_view->render('pages/'.$page.'.twig',$data));
+    $this->stop();
+  }
+  public function previewPage($template, $data) {
+    // Create temp file on disk to render the preview and then delete
+    $filename = '__preview_'.microtime(true).'.twig';
+    file_put_contents($this->config->sitedir.'/templates/'.$filename,$template);
+    $this->response->setBody($this->site_view->render($filename,$data));
+    unlink($this->config->sitedir.'/templates/'.$filename);
+    $this->stop();
+  }
+  public function getPageData($page) {
+    $data = array(
+      'meta'=>json_decode(file_get_contents($this->config->sitedir.'pages/'.$page.'/meta.json')),
+      'content'=>json_decode(file_get_contents($this->config->sitedir.'pages/'.$page.'/content.json')),
+      'contentschema'=>json_decode(file_get_contents($this->config->sitedir.'pages/'.$page.'/content.schema.json')),
+      'template'=>file_get_contents($this->config->sitedir.'templates/pages/'.$page.'.twig')
+    );
+    
+    return $data;
+  }
+  public function getSiteParameters() {
+    return json_decode(file_get_contents($this->config->sitedir.'parameters.json'));
+  }
+}
 
-$app = new \Slim\Slim(array(
+$app = new DornCMSApp(array(
     'view' => new \Slim\Views\Twig()
 ));
 
 $view = $app->view();
+$view->setTemplatesDirectory('admin_templates');
 $view->parserExtensions = array(
     new \Slim\Views\TwigExtension(),
 );
@@ -24,10 +74,10 @@ $app->get('/admin/login',function() use($app) {
   $app->render('admin/login.twig',array());
 })->name('login');
 
-$app->post('/admin/login',function() use($app, $config) {
+$app->post('/admin/login',function() use($app) {
   $post = (object) $app->request()->post();
   if(isset($post->username) && isset($post->password)) {
-    foreach($config->users as $user) {
+    foreach($app->config->users as $user) {
       if($user->username === $post->username && $user->password === $post->password) {        
         $_SESSION['authenticated'] = $post->username;
         $app->redirect($app->urlFor('admin'));
@@ -53,21 +103,17 @@ $app->post('/admin/preview', $authenticate, function() use($app) {
   $post = $app->request()->post();
   
   $parameters = '';
-  if(isset($post['parameters'])) $parameters = $post['parameters'];
-  else $parameters = file_get_contents('site/parameters.json');
+  if(isset($post['parameters'])) $parameters = json_decode($post['parameters']);
+  else $parameters = $app->getSiteParameters();
 
   $data = array(
     'meta'=>json_decode($post['meta']),
     'content'=>json_decode($post['content']),
-    'parameters'=>json_decode($parameters)
+    'parameters'=>$parameters
   );
   
-  // Create temp file on disk to render the preview and then delete
-  $filename = '__preview_'.microtime(true).'.twig';
-  file_put_contents('templates/'.$filename,json_decode($post['template'])->template);
-  $app->render($filename,$data);
-  unlink('templates/'.$filename);
-});
+  $app->previewPage(json_decode($post['template'])->template,$data);
+})->name('admin_preview');
 
 $app->get('/admin', $authenticate, function() use($app) {
   $pages = scandir('site/pages/');
@@ -88,35 +134,28 @@ $app->get('/admin/page/:page', $authenticate, function($page) use($app) {
 })->name('edit_page');
 
 $app->get('/admin/api/page/:page', $authenticate, function($page) use($app) {
-  // Get template data
-  $data = array(
-    'meta'=>json_decode(file_get_contents('site/pages/'.$page.'/meta.json')),
-    'metaschema'=>json_decode(file_get_contents('config/meta.schema.json')),
-    'content'=>json_decode(file_get_contents('site/pages/'.$page.'/content.json')),
-    'contentschema'=>json_decode(file_get_contents('site/pages/'.$page.'/content.schema.json')),
-    'template'=>file_get_contents('templates/pages/'.$page.'.twig')
-  );
+  $data = $app->getPageData($page);
+  $data['metaschema'] = json_decode(file_get_contents('config/meta.schema.json'));
   
   $res = $app->response;
   $res->setBody(json_encode($data));
-});
+})->name('admin_api_page');
+
 $app->post('/admin/api/page/:page', $authenticate, function($page) use($app) {
   // TODO: save page data
-});
+})->name('admin_api_page_post');
 
-$displayPage = function($page) use($app) {
-  // Get template data
-  $data = array(
-    'meta'=>json_decode(file_get_contents('site/pages/'.$page.'/meta.json')),
-    'content'=>json_decode(file_get_contents('site/pages/'.$page.'/content.json')),
-    'parameters'=>json_decode(file_get_contents('site/parameters.json'))
-  );
-  
-  $app->render('pages/'.$page.'.twig',$data);
-};
+// Custom routes
+if(file_exists($app->config->sitedir.'routes.php')) {
+  require($app->config->sitedir.'routes.php');
+}
 
-$app->get('/',function() use($displayPage) { $displayPage('home'); })->name('home');
+$app->get('/',function() use($app) { 
+  $app->renderSitePage($app->config->home);
+})->name('home');
 
-$app->get('/:page', $displayPage); 
+$app->get('/:page', function($page) use($app) {
+  $app->rednerSitePage($page);
+})->name('page');
 
 $app->run(); 
